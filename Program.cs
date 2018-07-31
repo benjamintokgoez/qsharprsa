f39fb8d60b62454e23790d8bfa07ecc8db0cb102
benjamintokgoez <benjamin.tokgz@microsoft.com>
2018-07-31 09:07:07 +0200

    creating basic csharp projekt

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace qsharprsa
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
