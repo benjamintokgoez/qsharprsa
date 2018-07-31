7c7e1bd9c4dd1623d55420c3e7a7e87afef0fbc3
benjamintokgoez <benjamin.tokgz@microsoft.com>
2018-07-31 09:14:08 +0200

    added basic qsharp project

---
+++ qsharp/Driver.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.Quantum.Simulation.Core;
+using Microsoft.Quantum.Simulation.Simulators;
+
+namespace qsharp
+{
+    class Driver
+    {
+        static void Main(string[] args)
+        {
+
+        }
+    }
+}
---
+++ qsharp/Operation.qs
@@ -0,0 +1,13 @@
+﻿namespace qsharp
+{
+    open Microsoft.Quantum.Canon;
+    open Microsoft.Quantum.Primitive;
+
+    operation Operation () : ()
+    {
+        body
+        {
+            
+        }
+    }
+}
