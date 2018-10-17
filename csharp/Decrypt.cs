7d69cfc7e552fac1f73ef520bdd05e2a5b8caa3a
benjamintokgoez <benjamin.tokgz@microsoft.com>
2018-10-17 12:48:55 +0200

    fixed c# sample source code for decryption

--- csharp/Decrypt.cs
+++ csharp/Decrypt.cs
@@ -7,33 +7,19 @@ namespace qsharprsa
     {
         int p=0;
         int q=0;
-        public double decryptMessageWithKeys(int d, int n, int msg){
-            double temp = Math.Pow(msg, d);
-            double result = temp%n;
-
-
-            return result;
-        }
         //not finished/tested yet. prime factorization of n
-        //IMPORTANT: We will implement this simple brute force function in q#!
-        public int decryptMessageBruteForce(int n, int e ){
-            //TODO check for prime numbers before setting the result
-            for(int i=2; i<n/2; i++){
-                for(int j=2; j<n/2;j++){
+        //actually we are finding the factors of n
+        //IMPORTANT: We will implement this simple brute force function in q# via Shor's algorithm
+        public int decryptMessageKeyBruteForce(int n){
+            for(int i=2; i<n/2+1; i++){
+                for(int j=2; j<n/2+1;j++){
                     if(j*i == n){
                         p=i;
                         q=j;
                         break;
                     }
-                if(j*i == n){
-                        p=i;
-                        q=j;
-                        break;
                 }
-                }
-
             }
-
             return 1;
         }
     }
